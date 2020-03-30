b1f0252954a76c21d72cba9fa591ca01fc9478d3
Ilknur Mustafov <ilknurilko@gmail.com>
2020-03-30 21:54:25 +0200

    Little Contact App Practice Using React Props

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Ronaldinho",
+    imgURL:
+      "https://i.ytimg.com/vi/y4EG1R3BuDQ/maxresdefault.jpg",
+    phone: "+234 234 789",
+    email: "roniack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
